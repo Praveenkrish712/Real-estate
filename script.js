32bdd1b6121ef8888f7588bdfeb0dd23b961d20a
Praveenkrishnan <praveenkrish712@gmail.com>
2025-04-29 14:04:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const ul = document.querySelector(".header-menu nav ul")
+const responsive = document.querySelector(".responsive")
+
+responsive.addEventListener("click", () => {
+  ul.classList.toggle("show")
+})
+
+
+
+
