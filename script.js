470ff6572b0d2c4a35f8a2843780ee2f231d591e
Praveenkrishnan <praveenkrish712@gmail.com>
2025-05-07 14:09:35 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -17,3 +17,40 @@ function closemenu() {
   mobile.style.display="none";
   menulogo.style.display="block";
 }
+
+
+
+  document.querySelector('.form-button button').addEventListener('click', function () {
+    const requiredInputs = [
+      ...document.querySelectorAll('.inquiry-from2 input[type="text"]'),
+      ...document.querySelectorAll('.inquiry-from2 select')
+    ];
+
+    const isAgreementChecked = document.getElementById('Agreement').checked;
+
+    let allFilled = requiredInputs.every(input => input.value.trim() !== '');
+
+    if (!allFilled || !isAgreementChecked) {
+      alert("Please fill out all fields");
+    } else {
+      alert("Form submitted successfully");
+
+
+      requiredInputs.forEach(input => input.value = '');
+      document.getElementById('Agreement').checked = false;
+    }
+  });
+
+
+
+
+  document.getElementById('submitBtn').addEventListener('click', function () {
+    const email = document.getElementById('emailInput').value.trim();
+
+    if (email === "") {
+      alert("Please enter your email");
+    } else {
+      alert("Submitted successfully");
+      document.getElementById('emailInput').value = "";
+    }
+  });
